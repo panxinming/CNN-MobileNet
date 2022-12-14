c9dce419d859354f41040dd5bcb9959643ab606c
Xinming <471836430@qq.com>
2022-12-14 16:29:52 -0500

    CNN

---
+++ utils/MobileNet_Cell.py
@@ -0,0 +1,11 @@
+from keras.layers import BatchNormalization, DepthwiseConv2D, ReLU, Conv2D
+
+def mobilenet_block(x, f, s=1, alpha = 1):
+    x = DepthwiseConv2D(int(alpha * 3), strides=s, padding='same')(x)
+    x = BatchNormalization()(x)
+    x = ReLU()(x)
+
+    x = Conv2D(f, 1, strides=1, padding='same')(x)
+    x = BatchNormalization()(x)
+    x = ReLU()(x)
+    return x
--- utils/model.py
+++ utils/model.py
@@ -1,95 +1,38 @@
 # import packages we need
+import tensorflow as tf
+from keras.models import Sequential, Model
+from keras.layers import BatchNormalization, Dropout, Input, GlobalAvgPool2D, Dense, Conv2D
+from keras import backend as K
 
-from keras.optimizers import SGD, RMSprop, Adam, Nadam
-from keras.models import Sequential
-from keras.layers import Dense, Flatten, Conv2D, DepthwiseConv2D, AveragePooling2D, Activation
-from keras.layers import BatchNormalization, Dropout
-from keras import backend
-
-
-class Basic_CNN:
-    def __init__(self, input_dim, num_classes):
-        self.input_dim = input_dim
-        self.num_classes = num_classes
-        self.model = None
-        
-    def create_empty_model(self):
-        model = Sequential()
-        self.model = model
-    
-    def add_layer(self,use_batch = True, use_drop = True):
-        self.model.add(Conv2D(32, (3,3),strides=2))
-        self.model.add(DepthwiseConv2D((3,3), strides=1))
-
-        self.model.add(AveragePooling2D((7,7)))
-        self.model.add(Flatten())
-        self.model.add(Dense(self.num_classes, activation="softmax"))
-        
-    def train(self,x_train,y_train,optimizer,loss='sparse_categorical_crossentropy',val_split = 0.2,
-             epochs = 2, batch_size = 256):
-        
-        self.model.compile(optimizer=optimizer,loss=loss,metrics=['accuracy'])
-        self.model.fit(x=x_train, 
-                       y=y_train,
-                       validation_split = val_split,
-                       batch_size=batch_size,
-                       epochs=epochs)
-        
-    def summary(self):
-        return self.model.summary()
+from utils.MobileNet_Cell import *
 
 
-class mobilenet:
-    def __init__(self, input_dim, num_classes):
-        self.input_dim = input_dim
-        self.num_classes = num_classes
-        self.model = None
-        
-    def create_empty_model(self):
-        model = Sequential()
-        self.model = model
-    
-    def add_layer(self,use_batch = True, use_drop = True):
-        self.model.add(Conv2D(32, (3,3),strides=2))
-        self.model.add(DepthwiseConv2D((3,3), strides=1))
-        
-        self.model.add(Conv2D(64, (1,1),strides=1))
-        self.model.add(DepthwiseConv2D((3,3), strides=2))
-        
-        self.model.add(Conv2D(128, (1,1),strides=1))
-        self.model.add(DepthwiseConv2D((3,3), strides=1))  
-        
-        self.model.add(Conv2D(128, (1,1),strides=1))
-        self.model.add(DepthwiseConv2D((3,3), strides=2))         
-        
-        self.model.add(Conv2D(256, (1,1),strides=1))
-        self.model.add(DepthwiseConv2D((3,3), strides=1)) 
-        
-        self.model.add(Conv2D(256, (1,1),strides=1))
-        self.model.add(DepthwiseConv2D((3,3), strides=1))
-        
-        self.model.add(Conv2D(512, (1,1),strides=1))
-        for i in range(0,5):
-            self.model.add(DepthwiseConv2D((3,3), strides=1))
-            self.model.add(Conv2D(512, (1,1), strides=1))
-        self.model.add(DepthwiseConv2D((3,3), strides=2))
-        
-        self.model.add(Conv2D(1024, (1,1),strides=1))
-        self.model.add(DepthwiseConv2D((3,3), strides=2))
-        self.model.add(Conv2D(1024, (1,1),strides=1))
-        self.model.add(AveragePooling2D((7,7)))
-        self.model.add(Flatten())
-        self.model.add(Dense(self.num_classes, activation="softmax"))
-        
-    def train(self,x_train,y_train,optimizer,loss='sparse_categorical_crossentropy',val_split = 0.2,
-             epochs = 2, batch_size = 256):
-        
-        self.model.compile(optimizer=optimizer,loss=loss,metrics=['accuracy'])
-        self.model.fit(x=x_train, 
-                       y=y_train,
-                       validation_split = val_split,
-                       batch_size=batch_size,
-                       epochs=epochs)
-        
-    def summary(self):
-        return self.model.summary()
+
+# implement mobile net from scratch
+def mobilenet(input_shape, n_classes):  
+    input_1 = Input(input_shape)
+
+    x = Conv2D(32, 3, strides=2, padding='same')(input_1)
+    x = BatchNormalization()(x)
+    x = ReLU()(x)
+
+    x = mobilenet_block(x, 64)
+    x = mobilenet_block(x, 128, 2)
+    x = mobilenet_block(x, 128)
+
+    x = mobilenet_block(x, 256, 2)
+    x = mobilenet_block(x, 256)
+
+    x = mobilenet_block(x, 512, 2)
+    for _ in range(5):
+        x = mobilenet_block(x, 512)
+
+    x = mobilenet_block(x, 1024, 2)
+    x = mobilenet_block(x, 1024)
+  
+    x = GlobalAvgPool2D()(x)
+  
+    output = Dense(n_classes, activation='softmax')(x)
+  
+    model = Model(input_1, output)
+    return model
