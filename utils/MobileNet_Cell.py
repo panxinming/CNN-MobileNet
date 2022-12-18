991760ade53ec0b4a2320fc0e6e49a412d78e743
Junkai Ding <114452933+Junkai-Ding@users.noreply.github.com>
2022-12-18 15:56:59 -0500

    Add files via upload

--- utils/MobileNet_Cell.py
+++ utils/MobileNet_Cell.py
@@ -1,18 +1,18 @@
 from keras.layers import BatchNormalization, DepthwiseConv2D, ReLU, Conv2D
 
 def mobilenet_block(x, f, s=1, alpha = 1):
-    x = DepthwiseConv2D(int(alpha * 3), strides=s, padding='same')(x)
+    x = DepthwiseConv2D(3, strides=s, padding='same')(x)
     x = BatchNormalization()(x)
     x = ReLU()(x)
 
-    x = Conv2D(f, 1, strides=1, padding='same')(x)
+    x = Conv2D(int(f*alpha), 1, strides=1, padding='same')(x)
     x = BatchNormalization()(x)
     x = ReLU()(x)
     return x
 
 
 def Conv_block(x, f, s=1):
-    x = Conv2D(f, 3, strides=s, padding='same')(x)
+    x = Conv2D(3, strides=s, padding='same')(x)
     x = BatchNormalization()(x)
     x = ReLU()(x)
 
