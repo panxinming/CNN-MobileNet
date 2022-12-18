73be144105ecfb8f1d58c198092a970164219462
Antelofski <83829736+Antelofski@users.noreply.github.com>
2022-12-18 16:14:36 -0500

    Add files via upload

--- utils/MobileNet_Cell.py
+++ utils/MobileNet_Cell.py
@@ -1,6 +1,14 @@
 from keras.layers import BatchNormalization, DepthwiseConv2D, ReLU, Conv2D
 
 def mobilenet_block(x, f, s=1, alpha = 1):
+    """
+    construct a mobilenet block which includes DepthwiseConv 2D, Batch Normalization layer, and ReLU to the model
+    :x : shape of the input (height, width, channels)
+    :alpha : shrinking parameter
+    :f : number of filters, the number of channels after this layer will be int(alpha*n_filters)
+    :s : stride of the convolution
+    return the mobilenet block with multiple layers
+    """
     x = DepthwiseConv2D(3, strides=s, padding='same')(x)
     x = BatchNormalization()(x)
     x = ReLU()(x)
@@ -12,6 +20,14 @@ def mobilenet_block(x, f, s=1, alpha = 1):
 
 
 def Conv_block(x, f, s=1):
+    """
+    construct a mobilenet block which includes DepthwiseConv 2D, Batch Normalization layer, and ReLU to the model
+    :x : shape of the input (height, width, channels)
+    :alpha : shrinking parameter
+    :f : number of filters, the number of channels after this layer will be int(alpha*n_filters)
+    :s : stride of the convolution
+    return the convolution block with multiple layers
+    """
     x = Conv2D(3, strides=s, padding='same')(x)
     x = BatchNormalization()(x)
     x = ReLU()(x)
